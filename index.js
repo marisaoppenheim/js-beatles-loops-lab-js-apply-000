8590e4d456b70ab860f4ed277dda23c78100c8c6
Marisa Oppenheim <marisa.oppenheim@yahoo.com>
2018-02-15 18:12:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function theBeatlesPlay (musicians, instruments) {
   var result = [];
+  var string = '';
   for (var i = 0; i < musicians.length; i++) {
     var member = musicians[i];
     var play = instruments[i];
+    string = member + ' ' + 'plays' + ' ' + instrument
+
   }
 }
